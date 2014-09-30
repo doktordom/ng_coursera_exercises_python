07173836c9d860c892b81a175933c3233be77f98
Dominic Walliman <dwalliman@dwavesys.com>
2014-09-30 14:14:15 -0700

    Data plotting done.

--- ex2/logistic_regression.py
+++ ex2/logistic_regression.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from ex1.linear_regression import read_csv_data
 
 
-def plot_data_scatter(x_data, y_data, x_axis_label, y_axis_label, fit_x=[], fit_y=[]):
+def plot_data_scatter(x_data, y_data, data_labels, x_axis_label, y_axis_label, fit_x=[], fit_y=[]):
     """
     Plots data on a 2D scatter plot.
     :param x_data: A list of lists of the horizontal coordinates of the data.
@@ -13,11 +14,14 @@ def plot_data_scatter(x_data, y_data, x_axis_label, y_axis_label, fit_x=[], fit_
     :param x_fit:
     :param y_fit:
     """
-    for x, y in zip(x_data, y_data):
-        plt.scatter(x, y)
+    colors = iter(cm.rainbow(np.linspace(0, 1, len(x_data))))
+    for x, y, label in zip(x_data, y_data, data_labels):
+        plt.scatter(x, y, color=next(colors), label=label)
     plt.plot(fit_x, fit_y)
     plt.xlabel(x_axis_label)
     plt.ylabel(y_axis_label)
+    legend = plt.legend()  # Create the legend and then set it to be transparent.
+    legend.get_frame().set_alpha(0.5)
     plt.show()
 
 
@@ -26,7 +30,13 @@ def main():
     Runs the main code in this file.
     """
     data1 = read_csv_data('ex2data1.txt')
-    plot_data_scatter(data1[0], data1[1], 'Exam 1 score', 'Exam 2 score')
+    # Now need to separate the data into the two different sets.
+    x_data = [[data[0] for data in zip(data1[0], data1[2]) if int(data[1]) == 0]]
+    y_data = [[data[0] for data in zip(data1[1], data1[2]) if int(data[1]) == 0]]
+    x_data.append([data[0] for data in zip(data1[0], data1[2]) if int(data[1]) == 1])
+    y_data.append([data[0] for data in zip(data1[1], data1[2]) if int(data[1]) == 1])
+    data_labels = ['Not admitted', 'Admitted']
+    plot_data_scatter(x_data, y_data, data_labels, x_axis_label='Exam 1 score', y_axis_label='Exam 2 score')
 
 if __name__ == "__main__":
     main()
