27e480c817571a432f68a018f18fb57b2f8ae25a
doktordom <walliman@gmail.com>
2014-10-06 17:11:34 -0700

    Got logistic regression working ok. Did this by not normalizing the bias term in x and not normalizing y.

--- ex2/logistic_regression.py
+++ ex2/logistic_regression.py
@@ -4,6 +4,19 @@ import matplotlib.cm as cm
 from ex1.linear_regression import read_csv_data
 
 
+def line_plot(x, y, x_label, y_label):
+    """
+    :param x: list of the horizontal coordinates of the data.
+    :param y:  A list of the vertical coordinates of the data.
+    :param x_label: Label for the horizontal axis.
+    :param y_label: Label for the vertical axis.
+    """
+    plt.plot(x, y)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
+
+
 def plot_data_scatter(x_data, y_data, data_labels, x_axis_label, y_axis_label, fit_x=[], fit_y=[]):
     """
     Plots data on a 2D scatter plot.
@@ -46,12 +59,13 @@ def logistic_regression(x_data, y_data, alpha, num_iterations):
     """
     theta = np.zeros(x_data.shape[0])
     m = y_data.shape[0]
+    print 'm', m
     cost_per_epoch = []
     for epoch in range(num_iterations):
         # Calculate the hypothesis.
         hypothesis = sigmoid(theta, x_data)
         # Calculate the cost.
-        cost = (1/m) * np.sum((-y_data) * np.log(hypothesis) - (1-y_data) * np.log(1-hypothesis))
+        cost = (1./m) * np.sum((-y_data) * np.log(hypothesis) - (1-y_data) * np.log(1-hypothesis))
         cost_per_epoch.append(cost)
         print cost
         # Do the gradient update.
@@ -70,7 +84,8 @@ def admission_regression():
     x_data.append([data[0] for data in zip(data1[0], data1[2]) if int(data[1]) == 1])
     y_data.append([data[0] for data in zip(data1[1], data1[2]) if int(data[1]) == 1])
     data_labels = ['Not admitted', 'Admitted']
-    plot_data_scatter(x_data, y_data, data_labels, x_axis_label='Exam 1 score', y_axis_label='Exam 2 score')
+    # Plot the data.
+    # plot_data_scatter(x_data, y_data, data_labels, x_axis_label='Exam 1 score', y_axis_label='Exam 2 score')
 
     # Add bias array to original data.
     data1.insert(0, np.ones(data1[0].shape))
@@ -79,25 +94,21 @@ def admission_regression():
 
     # Normalize the data.
     normalization_constants = []
-    for column in x_data:
+    for column in x_data[1:]:  # Don't normalize the bias term.
         mean = np.mean(column)
         column -= mean  # Subtract mean.
         standard_deviation = np.std(column)
         if standard_deviation != 0.0:  # Avoid dividing by zero.
             column /= standard_deviation  # Divide by standard deviation.
         normalization_constants.append({'mean': mean, 'std': standard_deviation})
-    # Also normalize y_data.
-    y_mean = np.mean(y_data)
-    y_data -= y_mean
-    y_std = np.std(y_data)
-    y_data /= y_std
-    y_norms = {'mean': y_mean, 'std': y_std}
+    # Don't normalize the y_data because we know it is 0 or 1
 
     # Set hyper parameters.
-    alpha = 0.01  # Learning rate.
-    num_iterations = 10  # Number of updates before quitting.
+    alpha = 0.1  # Learning rate.
+    num_iterations = 2000  # Number of updates before quitting.
 
     optimal_theta, cost_per_epoch = logistic_regression(x_data, y_data, alpha, num_iterations)
+    line_plot(range(len(cost_per_epoch)), cost_per_epoch, 'Epoch', 'Cost')
 
 
 def main():
