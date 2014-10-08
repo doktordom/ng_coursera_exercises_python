Fixed the normalization issues. Now it is all working just fine.

ex2/logistic_regression.py
@@ -85,16 +85,16 @@ def admission_regression():
     y_data.append([data[0] for data in zip(data1[1], data1[2]) if int(data[1]) == 1])
     data_labels = ['Not admitted', 'Admitted']
     # Plot the data.
-    # plot_data_scatter(x_data, y_data, data_labels, x_axis_label='Exam 1 score', y_axis_label='Exam 2 score')
+    plot_data_scatter(x_data, y_data, data_labels, x_axis_label='Exam 1 score', y_axis_label='Exam 2 score')
 
     # Add bias array to original data.
     data1.insert(0, np.ones(data1[0].shape))
-    x_data = np.vstack(tuple(data1[:-1]))
-    y_data = data1[-1]
+    x_data_np = np.vstack(tuple(data1[:-1]))
+    y_data_np = data1[-1]
 
     # Normalize the data.
     normalization_constants = []
-    for column in x_data[1:]:  # Don't normalize the bias term.
+    for column in x_data_np[1:]:  # Don't normalize the bias term.
         mean = np.mean(column)
         column -= mean  # Subtract mean.
         standard_deviation = np.std(column)
@@ -107,8 +107,27 @@ def admission_regression():
     alpha = 0.1  # Learning rate.
     num_iterations = 2000  # Number of updates before quitting.
 
-    optimal_theta, cost_per_epoch = logistic_regression(x_data, y_data, alpha, num_iterations)
+    optimal_theta, cost_per_epoch = logistic_regression(x_data_np, y_data_np, alpha, num_iterations)
+    # Plot the cost per epoch.
     line_plot(range(len(cost_per_epoch)), cost_per_epoch, 'Epoch', 'Cost')
+    # Plot the original data with the decision boundary.
+    print optimal_theta
+    print normalization_constants
+
+    # Need to un-normalize theta, to get it back into the original form of the data.
+    orig_theta = [0] * 3
+    orig_theta[0] = optimal_theta[0] - \
+                    (optimal_theta[1]*normalization_constants[0]['mean']) / normalization_constants[0]['std'] - \
+                    (optimal_theta[2]*normalization_constants[1]['mean']) / normalization_constants[1]['std']
+    orig_theta[1] = optimal_theta[1] / normalization_constants[0]['std']
+    orig_theta[2] = optimal_theta[2] / normalization_constants[1]['std']
+    # Generate a fit to the data. Derived from y = h_theta(x) setting y = 0.5.
+    print 'x data', x_data
+    x1 = np.asarray(range(int(min(x_data[0])), int(max(x_data[0]))))
+    x2 = -(orig_theta[1] / orig_theta[2]) * x1 - (orig_theta[0] / orig_theta[2])
+    # x2 = -(optimal_theta_orig[1] / optimal_theta_orig[2]) * x1 + 130
+    plot_data_scatter(x_data, y_data, data_labels, x_axis_label='Exam 1 score', y_axis_label='Exam 2 score',
+                      fit_x=x1, fit_y=x2)
 
 
 def main():